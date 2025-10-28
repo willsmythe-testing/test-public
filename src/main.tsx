91e01ae2902d3bdaaea9f86d145c1e6e97b7d04d
willsmythe-testing <willsmythe+testing@github.com>
2025-10-28 12:57:49 -0400

    Add sign-in reminder message to main component

--- src/main.tsx
+++ src/main.tsx
@@ -6,5 +6,6 @@ import App from './App.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <div class="message">Please make sure to sign in.</div>
   </StrictMode>,
 )
